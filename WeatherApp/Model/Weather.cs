c9d49e94b7d3e61a8807adf2843bdeab7f70d2d2
GoPal <govind.s.22016@gmail.com>
2021-09-14 16:14:51 -0400

    Add files via upload
    weather app code

---
+++ WeatherApp/Model/Weather.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Model
+{
+    class Weather
+    {
+        public string Temperature { get; set; }
+        public string Precipitation { get; set; }
+    }
+}
